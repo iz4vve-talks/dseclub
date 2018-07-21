f08790b7ee9ee7ae1a4563ed4bba07f2c3488ac3
Pietro Mascolo <pietro_mascolo@optum.com>
2018-07-21 18:50:47 +0100

    ADDS initial elements

---
+++ code/data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+data_file = "/Users/pmascolo/Projects/personal/dseclub-clustering/data/time_series_bonds.xlsx"
+sheet_name = "d"
+index = "date"
+
+# START OMIT
+import pandas as pd
+
+data = pd.read_excel(data_file, sheet_name=sheet_name).set_index(index)
+# END OMIT
+
+data.columns = [f"X{i:03d}" for i in range(1, data.shape[1] + 1)]
+
+print(data.head())
+
